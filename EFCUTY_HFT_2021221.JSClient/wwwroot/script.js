a01d791dbd7f6a142d4b8f70f8f88097ebd84f6e
kiss.levente <kiss.levente@stud.uni-obuda.hu>
2022-04-23 17:19:15 +0200

    git push (continue work on another computer)

--- EFCUTY_HFT_2021221.JSClient/wwwroot/script.js
+++ EFCUTY_HFT_2021221.JSClient/wwwroot/script.js
@@ -1,3 +1,35 @@
-﻿fetch('http://localhost:54726/citizen')
+﻿let citizens = [];
+
+fetch('http://localhost:54726/citizen')
     .then(x => x.json())
-    .then(y => console.log(y));
+    .then(y => {
+        citizens = y;
+        console.log(citizens);
+        display();
+    });
+
+
+
+function display() {
+    citizens.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + 
+            t.personID + "</td><td>" +
+        t.name + "</td><td>" +
+            +
+            t.hasCriminalRecord + "</td><td>" + 
+            t.incomeInUSD + "</td><td>" + 
+            t.settlement.settlementName + "</td><td>" +
+            t.citizenship.name + "</td></tr>";
+
+        console.log(t.citizenship.name);
+    });
+}
+
+function toRow() {
+    let value = citizens.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr>" +
+            `<td>${t.personID}</td>` +
+    })
+}
