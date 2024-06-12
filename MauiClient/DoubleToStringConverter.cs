2791ce367cc70147f5d7330a067fd311dc305e95
kklevi96 <kklevi@gmail.com>
2024-06-12 09:55:03 +0200

    HDI double converter for xaml (settlement)

---
+++ MauiClient/DoubleToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace MauiClient
+{
+    public class DoubleToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue)
+            {
+                return doubleValue.ToString(culture);
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string stringValue && double.TryParse(stringValue, NumberStyles.Any, culture, out double doubleValue))
+            {
+                return doubleValue;
+            }
+            return value;
+        }
+    }
+}
